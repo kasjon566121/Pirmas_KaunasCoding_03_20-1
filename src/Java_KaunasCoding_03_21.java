bb0f900bbe6744c03e92cbd64c1b09efc10f35d6
kasjon566121 <kasparas.jonaitis4@gmail.com>
2018-03-21 19:28:26 +0200

    antra paskaita pirmi du uzdaviniai

---
+++ src/Java_KaunasCoding_03_21.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Java_KaunasCoding_03_21 {
+    // 1 uzdavinys lengvesnis
+    // ctrl + alt + l - formatuoja koda
+    public static void main(String[] args){
+        // sis objektas padeda nuskaitineti is konsoles
+        Scanner sc = new Scanner(System.in);
+
+        // paprasome, kad vartotojas ivestu skaiciu
+        System.out.println("Iveskite skaiciu");
+
+        // ivesto skaiciaus nuskaitymas
+        int numb = sc.nextInt();
+
+        // tikriname ar lyginis skaicius
+        if(numb % 2 == 0) {
+            System.out.println(numb + " yra lyginis");
+        } else {
+                System.out.println(numb + " nelyginis");
+            }
+
+    }
+}
---
+++ src/Java_KaunasCoding_03_21_2uzd.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Java_KaunasCoding_03_21_2uzd {
+
+    // 100 prilyginmas kaip MAX_NUMB
+    private static final int MAX_NUMB = 100;
+
+    // nuo cia pradedamas darbas
+    public static void main (String[] args){
+
+        // panasus i C Sharp Console.ReadLine()
+        // susikuriame ir naudojame visoje klaseje
+        // skirtas nuskaityti reiksmems
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Iveskite kiek skaiciu noresite priskirti");
+
+        // ivesto skaiciaus nuskaitymas
+        // laukia kol kazkas bus ivesta i konsole
+        int kiek = scanner.nextInt();
+
+        // kiek ilgio masyvas
+        int[] masyvas = new int[kiek];
+
+        // for ciklas
+        for (int i = 0; i < kiek; i++){
+            System.out.println("Iveskite " + i + " skaiciu");
+            // skaiciu nuskaitymas
+            // laukia kol vartotojas kazka ives i konsole
+            masyvas[i] = scanner.nextInt();
+        }
+
+        // metodo kvieitmas
+        // kadangi metodaas buvo static, kvieciame ji tiesiogiai per varda
+        printMasyvas(masyvas);
+    }
+
+    // naujas metodas
+    // spausdina tik tuos elementus kurie yra
+    private static void printMasyvas(int[] sk_Mas ){
+        // sis for yra atskiram metode, todel naudoja ne tipus, o klases
+        // tipas lokalus vardas : kintamasis is masyvo(pirmas, antras...)
+        for(Integer sk: sk_Mas){
+            // patikriname ar esamas skaicius yra didesnis uz 100
+            if(sk >= MAX_NUMB){
+                System.out.println(sk + " ");
+            }
+        }
+    }
+}
